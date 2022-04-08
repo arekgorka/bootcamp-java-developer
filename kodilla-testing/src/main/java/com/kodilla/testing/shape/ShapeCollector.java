005261e11b0e9651a017d4eb539c2953c51695e0
Arkadiusz Górka <arekgorka91@gmail.com>
2022-04-08 16:25:28 +0200

    6.4 - Shape Collector Test vol1

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,44 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector {
+
+
+    private List<Shape> shapes = new ArrayList<>();
+
+    public int getSize() {
+        return shapes.size();
+    }
+
+    public ShapeCollector() {
+    }
+
+    public void addFigure(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public void removeFigure(Shape shape) {
+        shapes.remove(0);
+        /*if (shapes.contains(shape)) {
+          shapes.remove(shape);
+        }*/
+    }
+
+    public Shape getFigure(int n) {
+        if (n<0 || n> shapes.size()) {
+            return null;
+        } else {
+            return shapes.get(n);
+        }
+    }
+
+    public String showFigures() {
+        String result = "";
+        for( Shape shape: shapes) {
+            result += shape.getShapeName();
+        }
+        return result;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,119 @@
+package com.kodilla.testing.shape;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@DisplayName("TDD: Shape Collection Test Suite")
+public class ShapeCollectorTestSuite {
+
+    @Nested
+    @DisplayName ("Test add and remove")
+        class AddAndRemove {
+
+        @Test
+        void testAddFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+
+            //When
+            shapeCollector.addFigure(new Square("Name-SmallSquare", "Field 8m2"));
+
+            //Then
+            Assertions.assertEquals(4,shapeCollector.getSize());
+        }
+
+        @Test
+        void testRemoveFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+
+            //When
+            shapeCollector.removeFigure(new Square("Name-Square", "Field 23m2"));
+
+            //Then
+            Assertions.assertEquals(2,shapeCollector.getSize());
+        }
+
+
+    }
+
+    @Nested
+    @DisplayName ("Test get and show")
+    class GetAndShow {
+
+        @Test
+        void testGetFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+
+            //When
+            //Shape expectedShape = shapeCollector.shapes.get(0);
+            Shape retrievedShape;
+            retrievedShape = shapeCollector.getFigure(0);
+
+            //Then
+            Assertions.assertNotNull(retrievedShape);
+            Assertions.assertEquals("Name-Square", retrievedShape.getShapeName());
+        }
+        @Test
+        void testGetFigureLessThan0() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+
+            //When
+            //Shape expectedShape = shapeCollector.shapes.get(0);
+            Shape retrievedShape;
+            retrievedShape = shapeCollector.getFigure(-1);
+
+            //Then
+            Assertions.assertNull(retrievedShape);
+        }
+
+        @Test
+        void testGetFigureGreaterThanListSize() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+
+            //When
+            //Shape expectedShape = shapeCollector.shapes.get(0);
+            Shape retrievedShape;
+            retrievedShape = shapeCollector.getFigure(5);
+
+            //Then
+            Assertions.assertNull(retrievedShape);
+        }
+
+        @Test
+        void testShowFigure() {
+            //Given
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(new Square("Name-Square", "Field 23m2"));
+            shapeCollector.addFigure(new Circle("Name-Circle", "Field 51m2"));
+            shapeCollector.addFigure(new Triangle("Name-Triangle", "Field 40m2"));
+            //When
+            String actual = shapeCollector.showFigures();
+            //Then
+            Assertions.assertEquals("Name-SquareName-CircleName-Triangle", actual);
+        }
+
+    }
+}
