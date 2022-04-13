61f5cb9b93aaa1505e8f88fb662071d4e51be96d
Arkadiusz Górka <arekgorka91@gmail.com>
2022-04-13 22:25:16 +0200

    7.2 immutable ForumUser vol1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -9,7 +10,7 @@ import java.util.Locale;
 public class StreamMain {
 
     public static void main(String[] args) {
-        /*ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10,5, (a,b) -> a+b);
@@ -21,12 +22,15 @@ public class StreamMain {
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);*/
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify( "Text To Beautify", text -> "ABC" + text + "ABC");
         poemBeautifier.beautify( "Text To Beautify", text -> text.toUpperCase());
         poemBeautifier.beautify( "Text To Beautify", text -> "@_" + text + "_@");
         poemBeautifier.beautify( "Text To Beautify", text -> text.toLowerCase());
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
